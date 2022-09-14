94174f9550e437104e7cde41254b95a8c82c3b18
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2022-09-15 00:58:00 +0700

    lmao

--- JS/helperFunction.js
+++
@@ -1,7 +0,0 @@
-degrees_to_radians = degrees => degrees * (Math.PI/180);
-
-randomRangeInt = (start, stop) =>
-  Math.round(Math.random() * (stop - start) + start);
-
-randomRangeFloat = (start, stop) =>
-  Math.random() * (stop - start) + start;
--- JS/init.js
+++
@@ -1,2 +0,0 @@
-const c = document.querySelector("canvas");
-const ctx = c.getContext("2d");
